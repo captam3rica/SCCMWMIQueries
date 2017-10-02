f544b068e4fa3c489caaf18c0ad70c4c8f773c34
captam3rica <mattwilsonsc@gmail.com>
2017-10-02 10:59:07 -0400

    this

--- microsoft_office_pro_plus_2010-2016.sql
+++ microsoft_office_pro_plus_2010-2016.sql
@@ -19,3 +19,32 @@ inner join SMS_G_System_CH_ClientSummary on SMS_G_System_CH_ClientSummary.Resour
 where (SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%Microsoft Office Professional Plus 2010%" or SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%Microsoft Office Professional Plus 2013%" or SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%Microsoft Office Professional Plus 2016%")
 
 order by SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName
+
+
+
+
+
+/*////////////////////////////////////////
+              2010 Only
+////////////////////////////////////////*/
+
+
+select distinct
+
+SMS_R_System.Name,
+SMS_R_System.LastLogonUserName,
+SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName,
+SMS_R_System.ClientType,
+SMS_G_System_CH_ClientSummary.LastPolicyRequest
+
+from SMS_R_System
+
+inner join SMS_G_System_ADD_REMOVE_PROGRAMS on
+SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId =
+SMS_R_System.ResourceId
+
+inner join SMS_G_System_CH_ClientSummary on SMS_G_System_CH_ClientSummary.ResourceID = SMS_R_System.ResourceId
+
+where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%Microsoft Office Professional Plus 2010%"
+
+order by SMS_R_System.Name
