7b0e1e683889058b4a326a26f480e309ec5b9443
captam3rica <mattwilsonsc@gmail.com>
2017-10-02 11:17:36 -0400

    Formatting

--- all_workstations.sql
+++ all_workstations.sql
@@ -1,12 +1,16 @@
 select distinct
 
 SMS_R_System.Name,
-SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.OperatingSystemNameandVersion,
-SMS_R_System.Build,
-SMS_R_System.Client,
+SMS_R_System.ResourceDomainORWorkgroup,
+SMS_R_System.DeviceOwner,
 SMS_R_System.LastLogonUserName,
+SMS_R_System.OperatingSystemNameandVersion,
+SMS_R_System.BuildNumber,
 SMS_R_System.IPAddresses,
 SMS_R_System.MACAddresses,
+SMS_R_System.ResourceId,
+SMS_R_System.ResourceType,
+SMS_R_System.Client,
 SMS_R_System.IsVirtualMachine,
 SMS_R_System.SMSUniqueIdentifier
 
